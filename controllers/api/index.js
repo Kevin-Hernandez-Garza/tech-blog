6740a41fae11bcf025c5d54ae2ccaf88d87da5ff
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-20 22:27:28 -0600

    CRUD methods on post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,13 @@
 // the index.js file works to collect all of the API routes and package them up 
-
-
 const router = require('express').Router();
 
+
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 
 // prefixing the api's with this endpoint inside quotes
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,7 +2,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// associations 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 // exporting data 
 module.exports = { User, Post };
