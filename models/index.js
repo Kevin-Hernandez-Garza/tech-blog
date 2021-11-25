b4b9fb419449dc844d488f256a4354ca00b5454e
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-24 18:04:22 -0600

    POST & VOTE through table

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // collecting data(importing models)
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // associations 
 User.hasMany(Post, {
@@ -11,5 +12,33 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote, 
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
 // exporting data 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
