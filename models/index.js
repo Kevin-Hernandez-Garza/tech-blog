694832c794f6368a1aed8a0516da13fdecd41ace
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-24 21:12:29 -0600

    comment model

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // associations 
 User.hasMany(Post, {
@@ -40,5 +41,21 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
 // exporting data 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
