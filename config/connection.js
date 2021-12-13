0268b0f8f1c12d2b48cf15b96eb23a71a5d25521
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-12-12 20:22:58 -0600

    updated code

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,6 @@ const Sequelize = require('sequelize');
 // making the .env file data available
 require('dotenv').config();
 
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   // creating connection to our db
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// }
-
 let sequelize;
 
 if(process.env.JAWSDB_URL) {
--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const exphbs = require('express-handlebars');
 // passing the helpers functions in this statement 
 const hbs = exphbs.create({ helpers });
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 //setting up express.js session and connects our session to our Sequelize database
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -22,19 +19,24 @@ const sess = {
   resave: false, 
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
+    db: sequelize,
+    checkExpirationInterval: 60 * 1000,
+    expiration: 60 * 60 * 1000
   })
 };
 
-app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
